66415449679666957dbe927220d12938bdb7df80
amesa0 <amesa062@fiu.edu>
2018-12-17 23:38:49 -0500

    danbooru api learning

--- projects/danbooru_thing/script.js
+++ projects/danbooru_thing/script.js
@@ -5,7 +5,6 @@ var page = 'https://danbooru.donmai.us/posts.json?page=' + page + '&tags=' + tag
 
 
 const app = document.getElementById('root');
-console.log(page);
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 app.appendChild(container);
@@ -17,10 +16,14 @@ request.onload = function () {
   var data = JSON.parse(this.response);
 
   if (request.status >= 200 && request.status < 400) {
+    
     data.forEach(post => {
     console.log(post.large_file_url);
-    if(post.large_file_url.endsWith("webm")) {
-        //do nothing  
+
+    var contains_webm = post.file_ext == 'webm';
+    var contains_loli = post.tag_string.includes("loli");
+    
+    if(contains_webm || contains_loli) {
     }else{
         const img = document.createElement('img');
         img.setAttribute('src', post.large_file_url);
